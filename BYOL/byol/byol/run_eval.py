2abb6c31613fbac76e667a8dd83fdcdb45b9fd87
johnnyj <joe27johnny@gmail.com>
2025-10-29 09:21:12 +0000

    Updated checkpoint path

--- BYOL/byol/byol/run_eval.py
+++ BYOL/byol/byol/run_eval.py
@@ -1,19 +1,14 @@
-print('------running eval code------')
 import logging
 import torch
 import pytorch_lightning as pl
 import wandb
-print('------ imported libraries')
 
 from paths import Path_Handler
-# from vae import CNNVAE
 from models import BYOL
 from evaluation import Linear_Eval, Lightning_Eval
 from datamodules import RGZ_DataModule
-print('------ imported py files')
 
 def run_linear_eval(config, encoder, datamodule, logger):
-    print('------running linear eval')
     ## Trainer and logger ##
     trainer = pl.Trainer(
         logger=logger,
@@ -24,30 +19,24 @@ def run_linear_eval(config, encoder, datamodule, logger):
     # Create the evaluation LightningModule
     model = Lightning_Eval(config)
     model.encoder = encoder 
-    print('------eval model acquired')
 
     # Run validation & test (evaluation is handled in the validation hooks)
     trainer.validate(model, datamodule=datamodule)
     trainer.test(model, datamodule=datamodule)
-
-    print('------linear eval done')
     return model
 
 
 def main():
-    print('------in main')
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s %(levelname)s: %(message)s",
     )
     paths = Path_Handler()._dict()
     
-    manual_ckpt_path = "/share/nas2_3/jalphonse/BYOL/byol/checkpoints/ajrehorg/last.ckpt"
+    manual_ckpt_path = "your_ckpt_path/last.ckpt" # path to BYOL model checkpoint
     byolmodel = BYOL.load_from_checkpoint(manual_ckpt_path)
     
     config = byolmodel.config
-    print('------byol model and config acquired')
-
 
     # Logging
     project_name = "BYOL_linear_eval"
@@ -77,7 +66,6 @@ def main():
         prefetch_factor=config["dataloading"]["prefetch_factor"],
         num_workers=config["dataloading"]["num_workers"],
         )
-        print('------Datamodule acquired')
 
         model = run_linear_eval(config, byolmodel.encoder, eval_datamodule, logger)
         all_models.append(model)
@@ -86,16 +74,12 @@ def main():
 
         # Log per-seed metrics
         wandb.log({f"eval/seed_{seed}_done": 1})
-        print(f"------ Evaluation complete for seed {seed}")
 
-    print("\nAll seeds evaluated:", all_seeds)
 
     wandb.log({"eval/seeds_completed": all_seeds})
 
     logger.experiment.finish()
     wandb.finish()
-    print('------Evaluation complete')
     
-
 if __name__ == "__main__":
     main()
