0c72126d617770cf9aea372a41fc6986e5b7ee18
johnnyj <joe27johnny@gmail.com>
2026-03-25 13:26:19 +0530

    Modify output and script paths in beta.sh
    Updated file paths in beta.sh to use a placeholder for user-defined paths.

--- beta.sh
+++ beta.sh
@@ -5,13 +5,13 @@
 #SBATCH --time=3-00
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
-#SBATCH --output=/share/nas2_3/jalphonse/VAE_RadioGalaxies/outs/output_beta/out_%j.out
+#SBATCH --output=/'your_path'/output_beta/out_%j.out
 #SBATCH --exclude compute-0-7
 
 pwd;
 
 nvidia-smi
 echo "--Env activated"
-source /share/nas2_3/jalphonse/anaconda3/bin/activate myenv
+source /'your_path'/anaconda3/bin/activate myenv
 echo "--Code Running"
-python /share/nas2_3/jalphonse/VAE_RadioGalaxies/beta_search.py
+python /'your_path'/beta_search.py
