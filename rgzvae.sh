9113a0b92e746ae94c961bb818e58dc7bf64fb5e
johnnyj <joe27johnny@gmail.com>
2026-03-25 13:27:12 +0530

    Modify output and script paths in rgzvae.sh
    Updated output path and script path in the SLURM script.

--- rgzvae.sh
+++ rgzvae.sh
@@ -4,13 +4,13 @@
 #SBATCH --time=8-00
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
-#SBATCH --output=/share/nas2_3/jalphonse/VAE_RadioGalaxies/outs/out_%j_mbkfold.out
+#SBATCH --output=/'your_path'/out_%j_mbkfold.out
 #SBATCH --exclude compute-0-7
 
 pwd;
 
 nvidia-smi
 echo "--Env activated"
-source /share/nas2_3/jalphonse/anaconda3/bin/activate myenv
+source /'your_path'/anaconda3/bin/activate myenv
 echo "--Code Running"
-python /share/nas2_3/jalphonse/VAE_RadioGalaxies/VAE_RGZ_mb16.py
+python 'your_path'/VAE_for_RGZData.py
